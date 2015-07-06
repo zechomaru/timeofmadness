9acd1ea52a1bf6ada821625717009033545c8b0d
xavier G <xavier457@hotmail.com>
2015-07-06 19:12:36 -0430

    v2.1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,13 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'timeofmadnees.com',
+      user_name:            'websprograming@gmail.com',
+      password:             'Compucredito1',
+      authentication:       'plain',
+      enable_starttls_auto: true  }
 end

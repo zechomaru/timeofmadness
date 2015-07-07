1f1df0bb703e480f3d4afccb9489089032bcfb89
xavier G <xavier457@hotmail.com>
2015-07-06 20:03:12 -0430

    v2.1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,6 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       address: "smtp.gmail.com",
       port: 587,
-      domain: 'timeofmadnees.com',
       authentication: "plain",
       enable_starttls_auto: true,
       user_name: 'websprograming@gmail.com',

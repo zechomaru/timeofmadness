8283266b510455dc546b0ff8058a88f369765e3d
xavier G <xavier457@hotmail.com>
2015-07-07 03:21:39 -0430

    v2.1

--- config/initializers/ads.rb
+++
@@ -1,8 +0,0 @@
-Ads.configure do |config|
-  config.renderer = lambda { |options|
-    tag(
-        :img,
-        src: "http://placehold.it/#{options[:width]}x#{options[:height]}&text=Adsense"
-    )
-  }
-end
